11312292af26db8c5472ba48b8087b19fd85c4c1
pflis610 <piotrek610@wp.pl>
2021-08-02 14:49:23 +0200

    zadanie34 dopieszczenie

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 let str1 = 'Uwielbiam JavaScript';
 let str2 = 'Jestem świetnym programistą';
-let stringcheck = function (str1, str2) {
-if (str1.length > str2.length) {
-    return str1;
-} else {
-    return str2;
-    }
+let stringcheck = function (string1, string2) {
+    if (string1.length > string2.length) {
+    return string1;
+    } else {
+      return string2;
+      }
 }
 console.log(stringcheck(str1, str2));
